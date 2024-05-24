403f6eabb438fd78c3647fd13962715d5d7e5a64
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-05-24 09:17:31 +0700

    update: signin route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const { PrismaClient } = require("@prisma/client");
-// const jwt = require('jsonwebtoken');
-// require('dotenv').config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const prisma = new PrismaClient();
 
@@ -29,8 +29,48 @@ const signup = async (req, res, next) => {
     }
 };
 
+const signin = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+
+        // Find the user by username
+        const user = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        });
+
+        // If user does not exist, return an error
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        // Compare the provided password with the hashed password stored in the database
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+        // If the password is invalid, return an error
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        // Generate a JWT token
+        const token = jwt.sign(
+            { id: user.id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        // Send the token and user details in the response
+        res.json({ status: "ok", message: "signin success", token });
+    } catch (error) {
+        next(error);
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
 module.exports = {
     signup,
-    // signin,
+    signin,
     // signout
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { signup } = require('../controllers/authController');
+const { signup, signin } = require('../controllers/authController');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const router = express.Router();
 
 router.post('/signup', jsonParser, signup);
-// router.post('/signin', signin);
+router.post('/signin', jsonParser, signin);
 // router.get('/signout', signout);
 
 module.exports = router;
